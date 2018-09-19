3487e75ecdd6fc0b379739dbdba024110c1309b8
Kyle MacMillan <macattackftw@gmail.com>
2018-09-19 13:28:24 -0600

    Made adjustments to 4.13.

--- HW2/Problem4_13.py
+++ HW2/Problem4_13.py
@@ -2,63 +2,84 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose2D
+import numpy as np
 
-pt0 = (0.0, 0.0)
-pt1 = (0.0, 15.0)
-pt2 = (5.0, 20.0)
 
-theta0 = 0.0
-theta1 = 0.0
-theta2 = 0.0
+class DiffDrive():
+    def __init__(self):
+        self.pt0 = (0.0, 0.0)
+        self.pt1 = (0.0, 15.0)
+        self.pt2 = (5.0, 20.0)
 
-dist_threshold = 1.0
+        # Radians
+        self.theta0 = 0.0
+        self.theta1 = 0.0
+        self.theta2 = 0.0
+        self.dist_threshold = 1.0
+        self.rosinit()
 
-def dist(pt, xy):
-    pass
+    def rosinit(self):
+        node_name = 'circle'
+        rclpy.init()
+        node = Node(node_name)
 
-def thetadiff(goal, theta):
-    return True
+        print('Spinning: {}'.format(node_name))
+        self.publeft = node.create_publisher(Float32, '/wheel_left')
+        self.pubright = node.create_publisher(Float32, '/wheel_right')
+        pos = node.create_subscription(Pose2D, '/GPS', self.callback)
 
-def callback(msg):
-    print('x,y:   {},{}'.format(msg.x, msg.y))
-    print('theta: {}'.format(msg.theta))
-    if dist(pt0, (msg.x, msg.y)) < dist_threshold and thetadiff(theta0, msg.theta):
-        # Rotate
-        print('rotate')
-    else:
-        # Have obtained correct angle, now move forward
-        new_msg = Float32()
-        new_msg.data = 2.0
-        publeft.publish(new_msg)
-        pubright.publish(new_msg)
+        msg = Float32()
+        msg.data = 2.0
+        self.publeft.publish(msg)
+        self.pubright.publish(msg)
+
+        try:
+            rclpy.spin(node)
+        except KeyboardInterrupt:
+            print('Shutting down...')
+            msg.data = 0.0
+            self.publeft.publish(msg)
+            self.pubright.publish(msg)
+            node.destroy_node()
+            rclpy.shutdown()
+
+    def dist(self, pt, xy):
+        return np.sqrt((pt[0] - xy[0] * pt[0] - xy[0]) + (pt[1] - xy[1] * pt[1] - xy[1]))
+
+    def thetadiff(self, goal, theta):
+        return np.absolute(theta - goal)
+
+    def callback(self, msg):
+        print('x,y:   {},{}'.format(msg.x, msg.y))
+        print('theta: {}'.format(msg.theta))
+        dist = self.dist(self.pt1, (msg.x, msg.y))
+        print('dist: {}'.format(dist))
+        thetadist = self.thetadiff(self.theta1, msg.theta)
+        print('thetadist: {}'.format(thetadist))
+
+        # Naive approach
+        if dist < self.dist_threshold and thetadist < self.dist_threshold:
+            # Rotate
+            print('rotate')
+            new_msg = Float32()
+            new_msg.data = 0.0
+            self.publeft.publish(new_msg)
+            new_msg.data = -0.0
+            self.pubright.publish(new_msg)
+        else:
+            # Have obtained correct angle, now move forward
+            new_msg = Float32()
+            new_msg.data = 2.0
+            self.publeft.publish(new_msg)
+            self.pubright.publish(new_msg)
 
 
 
 if __name__ == '__main__':
+    DiffDrive()
 
 
-    node_name = 'circle'
-    rclpy.init()
-    node = Node(node_name)
-
-    print('Spinning: {}'.format(node_name))
-    publeft = node.create_publisher(Float32, '/wheel_left')
-    pubright = node.create_publisher(Float32, '/wheel_right')
-    pos = node.create_subscription(Pose2D, '/GPS', callback)
     
 
-    msg = Float32()
 
-    msg.data = 2.0
-    publeft.publish(msg)
-    pubright.publish(msg)
 
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        print('Shutting down...')
-        msg.data = 0.0
-        publeft.publish(msg)
-        pubright.publish(msg)
-        node.destroy_node()
-        rclpy.shutdown()

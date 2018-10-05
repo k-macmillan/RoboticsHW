cf32695ff0d1dd11f12b0b6aa91cefb870c5f133
Kyle MacMillan <macattackftw@gmail.com>
2018-10-05 12:37:09 -0600

    Renamed robot0 to RosController. Homework problems will be derivations of that class so I know rclpy setup and cleanup are taken care of.

---
+++ HW3/Problem2.py
@@ -0,0 +1,42 @@
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Pose2D
+from RosController import *
+
+
+class Problem2(RosController):
+    def __init__(self):
+        super(Problem2, self).__init__()
+        self.setupWheels()
+        self.setupGPS()
+
+    def setupWheels(self):
+        wheels = makeNode('Wheels')
+        self.pub_left = wheels.create_publisher(Float32,
+                                                '/robot0/wheel_left')
+        self.pub_right = wheels.create_publisher(Float32,
+                                                 '/robot0/wheel_right')
+        self.msg_wheels = Float32()
+        self.setVel(2.0, 2.0)
+
+    def setVel(self, left=None, right=None):
+        if left is not None:
+            self.msg_wheels.data = left
+            self.pub_left.publish(self.msg_wheels)
+        if right is not None:
+            self.msg_wheels.data = right
+            self.pub_right.publish(self.msg_wheels)
+
+    def setupGPS(self):
+        makeNode('GPS')
+        self.gps.create_subscription(Pose2D,
+                                     '/robot0/GPS',
+                                     self.gpsPublish)
+
+    def gpsPublish(self, msg):
+        print('x,y:   {}, {}'.format(msg.x, msg.y))
+        print('theta: {}'.format(msg.theta))
+
+
+if __name__ == '__main__':
+    p2 = Problem2()
+    p2.run()
---
+++ HW3/RosController.py
@@ -0,0 +1,47 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import SingleThreadedExecutor
+
+
+class RosController(object):
+    def __init__(self):
+        try:
+            rclpy.init()
+            print('rclpy initialized...')
+        except BaseException:
+            pass
+        self.executor = SingleThreadedExecutor()
+        self.nodes = []
+        self.timers = []
+
+    def makeNode(self, name):
+        node = Node(name)
+        self.nodes.append(node)
+        print('{} node created...'.format(name))
+        return node
+
+    def __shutdown(self):
+        print('\nCleaning up...')
+        try:
+            self.setVel(0.0, 0.0)
+            for node in self.nodes:
+                name = node.get_name()
+                node.destroy_node()
+                print('Destroyed {}...'.format(name))
+
+            self.executor.shutdown()
+            rclpy.shutdown()
+            print('Shut down...')
+        except BaseException:
+            # Already shutdown
+            pass
+
+    def run(self):
+        for node in self.nodes:
+            self.executor.add_node(node)
+
+        try:
+            self.executor.spin()
+        except KeyboardInterrupt:
+            self.__shutdown()
+            exit(1)

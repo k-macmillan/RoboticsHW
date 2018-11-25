c83aa6102fba38810026feda9205f21568429043
Kyle MacMillan <macattackftw@gmail.com>
2018-11-24 21:38:38 -0700

    Updated 17.3 a little. Chipping away at it...

--- HW5/problem17_3.py
+++ HW5/problem17_3.py
@@ -3,10 +3,18 @@ import numpy as np
 
 # Time information
 dt = 0.1
-t = np.arange(0, 2 * np.pi + dt, dt)
+t = np.arange(0, 2 * np.pi + dt, dt)  # 64 points
 
 # x_dot = y
 # y_dot = -np.cos(t) + 0.5 * np.sin(t)
 
-# Variance
-w = 0.25  # x
+x = np.zeros(len(t))
+
+# "Physics"
+F = np.array([1.0, 0.1],
+             [0.1 * np.sin(x), 1.0])
+
+# Variances
+V = np.array([0.2, 0.0],
+             [0.0, 0.2])
+W = 0.25
